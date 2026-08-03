01cb55c3cab48b2c29b53ad17f42b84622f52ddc
Keyaan <keyaangame@gmail.com>
2026-08-03 03:21:06 -0400

    FUCK YEAEHHH WORKING BOILED ETHERSTONE STRIPPING

--- src/main/java/com/keyaan/BoiledEtherstoneResinRemove.java
+++ src/main/java/com/keyaan/BoiledEtherstoneResinRemove.java
@@ -1,40 +1,63 @@
 package com.keyaan;
 
 import com.mojang.logging.LogUtils;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ToolMaterial;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 
-public class BoiledEtherstoneResinRemove extends AxeItem {
-    private static final Logger LOGGER =  LogUtils.getLogger();
-
-    public BoiledEtherstoneResinRemove(
-            ToolMaterial material,
-            float attackDamage,
-            float attackSpeed,
-            Item.Properties properties
-    ) {
-        super(material, attackDamage, attackSpeed, properties);
-    }
-
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        BlockPos pos = context.getClickedPos();
-        LOGGER.info("An Axe was used on anything!");
-        BlockState state = context.getLevel().getBlockState(pos);
-        if (state.getBlock() == EnditiumBlocks.BOILED_RESIN_ETHERSTONE) {
-            LOGGER.info("Axe was used on Boiled Resin Etherstone!");
-        }
-        return InteractionResult.SUCCESS;
-
-    }
-
+public class BoiledEtherstoneResinRemove {
     public static void Init() {
-        LOGGER.info("Initialized Script for Axe and Boiled Etherstone!!");
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            //player right clicked on a block
+            //Whats in the players hand?
+            ItemStack itemInHand = player.getItemInHand(hand);
+            //Check if you have an axe
+            if (itemInHand.is(ItemTags.PICKAXES)) {
+                //The item is an axe
+                //Check what block's actually?
+                BlockPos pos = hitResult.getBlockPos();
+                Block BlockClicked = world.getBlockState(pos).getBlock();
+                if (BlockClicked == EnditiumBlocks.BOILED_RESIN_ETHERSTONE) {
+                    ItemStack EtherResinNugget = new ItemStack(EnditiumItems.ETHER_RESIN_NUGGET);
+                    //Visuals and Audio
+                    world.addDestroyBlockEffect(pos, EnditiumBlocks.BOILED_ETHERSTONE.defaultBlockState());
+                    world.playSound(player, pos, SoundEvents.HONEY_BLOCK_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    world.playSound(player, pos, SoundEvents.CALCITE_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    player.swing(hand);
+                    //Functionallity
+                    world.setBlock(pos, EnditiumBlocks.BOILED_ETHERSTONE.defaultBlockState(), Block.UPDATE_ALL);
+                    //the position of where the item should be
+                    Direction face = hitResult.getDirection();
+                    Vec3 facedirection = new Vec3(
+                            face.getStepX(),
+                            face.getStepY(),
+                            face.getStepZ()
+                    );
+                    //Spawn the item
+                    ItemEntity SpawnedResin = new ItemEntity(world, pos.getX()+facedirection.x, pos.getY()+facedirection.y, pos.getZ()+facedirection.z, EtherResinNugget);
+                        //then set the velocity
+                        Vec3 rawDirection = player.position().subtract(SpawnedResin.position());
+                        Vec3 direction = rawDirection.normalize();
+                        SpawnedResin.setDeltaMovement(direction.scale(0.15));
+                        world.addFreshEntity(SpawnedResin);
+                    return InteractionResult.SUCCESS;
+                }
+            }
+
+            return InteractionResult.PASS;
+        });
     }
 }
