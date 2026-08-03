91646bc5490f19ea54125527362b0314e55049c9
Keyaan <keyaangame@gmail.com>
2026-08-03 17:39:40 -0400

    Enditium all recipies in!!!

--- src/main/java/com/keyaan/EnditiumItems.java
+++ src/main/java/com/keyaan/EnditiumItems.java
@@ -3,10 +3,15 @@ package com.keyaan;
 
 import com.bananaman.api.RegistryHelper;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.SmithingTemplateItem;
+
+import java.util.List;
 
 
 public class EnditiumItems {
@@ -55,6 +60,32 @@ public class EnditiumItems {
                     new Item.Properties()
                             .rarity(Rarity.EPIC)
             );
+
+    public static final Item ENDITIUM_UPGRADE_TEMPLATE = registryHelper.registerItem(
+            "enditium_upgrade_template",
+            properties -> {
+                
+                return new SmithingTemplateItem(
+                        Component.translatable("item.enditium.ether_upgrade.applies_to")
+                                .withStyle(style -> style.withColor(0x55FFFF)),
+                        Component.translatable("item.enditium.ether_upgrade.ingredients")
+                                .withStyle(style -> style.withColor(0x55FFFF)),
+                        Component.translatable("item.enditium.ether_upgrade.base_slot"),
+                        Component.translatable("item.enditium.ether_upgrade.addition_slot"),
+                        List.of(
+                                Identifier.fromNamespaceAndPath("minecraft", "container/slot/helmet"),
+                                Identifier.fromNamespaceAndPath("minecraft", "container/slot/chestplate"),
+                                Identifier.fromNamespaceAndPath("minecraft", "container/slot/leggings"),
+                                Identifier.fromNamespaceAndPath("minecraft", "container/slot/boots")
+                                ),
+                        List.of(
+                                Identifier.fromNamespaceAndPath("minecraft", "container/slot/ingot")
+                        ),
+                        properties
+                );
+            },
+            new Item.Properties().rarity(Rarity.EPIC)
+    );
 
     public static void init() {
 
