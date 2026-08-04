b9a0a1ac517536a80c8b1f9b67ba38998a4d669a
Keyaan <keyaangame@gmail.com>
2026-08-04 04:14:25 -0400

    spike spawning progres

--- src/main/java/com/keyaan/SpikesSpawn.java
+++ src/main/java/com/keyaan/SpikesSpawn.java
@@ -8,6 +8,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
@@ -25,32 +26,40 @@ public class SpikesSpawn {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public static void placeSpike(ServerLevel world, BlockPos pos) {
+        for(int tries = 0; tries < 20; tries++) {
         int spikeNumberToSpawn = world.getRandom().nextInt(1, 5);
         String spikeToSpawn = "etherstone_spike_" + spikeNumberToSpawn;
         //Get the structure manager from the server
         StructureTemplateManager structureManager = world.getServer().getStructureManager();
         //Make sure the spike exists
-        LOGGER.info("Checking if the file exists");
-
         var SpikeCheck = structureManager.get(
+                //Randomize the spike to spawn in the process
                 Identifier.fromNamespaceAndPath(Enditium.MOD_ID, spikeToSpawn)
         );
-
-        LOGGER.info("Structure exists? {}", SpikeCheck.isPresent());
         if (!SpikeCheck.isEmpty()) {
-            //If it do, create a var w/ it
+            //If it do, randomize the positions
+            int x = pos.getX()+world.getRandom().nextInt(30, 100);
+            int z = pos.getZ()+world.getRandom().nextInt(30, 100);
+            int y = world.getHeight(Heightmap.Types.WORLD_SURFACE, x, z);
+            //Turn the structuretemplatemanager into a structure template
             StructureTemplate Spike = SpikeCheck.get();
-            BlockPos SpikePos = new  BlockPos(pos.getX(), pos.getY()-5, pos.getZ());
-            LOGGER.info("Placed Spike 1");
-            Spike.placeInWorld(
-                    world,
-                    SpikePos,
-                    pos,
-                    new StructurePlaceSettings()
-                            .setRotation(Rotation.getRandom(world.getRandom())),
-                    world.getRandom(),
-                    Block.UPDATE_ALL
-                    );
+            //Finally, place the structure!
+            BlockPos SpikePos = new  BlockPos(x, y-5, z);
+            if (!world.getBlockState(SpikePos).isAir()) {
+                Spike.placeInWorld(
+                        world,
+                        SpikePos,
+                        pos,
+                        new StructurePlaceSettings()
+                                .setRotation(Rotation.getRandom(world.getRandom())),
+                        world.getRandom(),
+                        Block.UPDATE_ALL
+                );
+            }
+            else {
+                placeSpike(world, pos);
+            }
+            }
         }
     }
-}
+};
